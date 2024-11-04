2233dab380523147c2bc4884e86a220e664eaaa4
fareed2647 <fareed2647@gmail.com>
2024-11-04 12:52:18 +0530

    removed cal_sal function

--- api.py
+++ api.py
@@ -7,11 +7,5 @@ def hii():
     data = "hello how can i hlep you"
     return data
 
-@app.route('/cal_sal', methods = ['GET']) 
-def mrg():
-    data = "Provide me salary details"
-    return data
-
-
 if __name__ == '__main__':
     app.run(debug= True )
