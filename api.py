67d3caa5af735ada8b3705d4b2acf3064f4cfdaf
fareed2647 <fareed2647@gmail.com>
2024-11-04 15:33:50 +0530

    incentive function added

--- api.py
+++ api.py
@@ -7,5 +7,12 @@ def hii():
     data = "hello how can i hlep you"
     return data
 
+
+@app.route('/inc', methods = ['GET']) 
+
+def incentive():
+    data = 500
+    return data
+
 if __name__ == '__main__':
     app.run(debug= True )
