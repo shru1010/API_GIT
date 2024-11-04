c8abc0d9845966f52ceecb506b9fab1d07683e86
fareed2647 <fareed2647@gmail.com>
2024-11-04 15:49:06 +0530

    restoring

--- api.py
+++ api.py
@@ -1,26 +1,18 @@
 from flask import Flask,jsonify,request
 
 app = Flask(__name__) 
-@app.route('/hii', methods = ['GET']) 
 
+
+@app.route('/hii', methods = ['GET']) 
 def hii():
     data = "hello how can i hlep you"
     return data
 
-<<<<<<< HEAD
-@app.route('/sal', methods = ['GET']) 
-def sal():
-    salary = 30000
-    return salary
-
-=======
 
 @app.route('/inc', methods = ['GET']) 
-
 def incentive():
     data = 500
     return data
->>>>>>> incentive
 
 if __name__ == '__main__':
     app.run(debug= True )
